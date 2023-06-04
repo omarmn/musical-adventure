8cc95a35cab3a1c01af0de972500250a8ad6e1da
Omar Nooreddin <12260449+omarmn@users.noreply.github.com>
2023-06-04 22:14:35 +0100

    Add files via upload

---
+++ Untitled11.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import pandas as pd
+import streamlit as st
+
+# Read the CSV file
+data = pd.read_excel('Matrice Afidium copie.xlsx', sheet_name='RAW DATA1')
+
+
+# In[33]:
+
+skills=data.columns[1:].tolist()
+skills.sort()
+column_name=st.selectbox('Select a skill', skills)
+top3=data[column_name].sort_values(ascending=False)[:3].index
+st.dataframe(data['Ton prénom/nom'].loc[top3])
+
