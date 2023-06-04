760dbe03294bf7cfbac1d08a15556b4c07ef99ec
Omar Nooreddin <12260449+omarmn@users.noreply.github.com>
2023-06-04 22:48:23 +0100

    Update Untitled11.py

--- Untitled11.py
+++ Untitled11.py
@@ -18,5 +18,7 @@ skills=data.columns[1:].tolist()
 skills.sort()
 column_name=st.selectbox('Select a skill', skills)
 top3=data[column_name].sort_values(ascending=False)[:3].index
-st.dataframe(data['Ton prénom/nom'].loc[top3])
+df=data[['Ton prénom/nom',column_name]].loc[top3]
+df.rename(columns={column_name:'Score'}, inplace=True)
 
+st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
