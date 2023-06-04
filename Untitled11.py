54657df853d81a456c389e207e51636ea05b6c63
Omar Nooreddin <12260449+omarmn@users.noreply.github.com>
2023-06-04 23:15:55 +0100

    Update Untitled11.py

--- Untitled11.py
+++ Untitled11.py
@@ -17,8 +17,9 @@ data = pd.read_excel('Matrice Afidium copie.xlsx', sheet_name='RAW DATA1')
 skills=data.columns[1:].tolist()
 skills.sort()
 column_name=st.selectbox('Select a skill', skills)
-top3=data[column_name].sort_values(ascending=False)[:5].index
+top3=data[column_name].sort_values(ascending=False)[:3].index
 df=data[['Ton prénom/nom',column_name]].loc[top3]
-df.rename(columns={column_name:'Score'}, inplace=True)
+# df.rename(columns={column_name:'Score'}, inplace=True)
+df.columns=['prénom/nom', 'Score']
 
 st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
