c6bc238d3be9a3f1952c5ee6920131a2ee0d25a8
Omar Nooreddin <12260449+omarmn@users.noreply.github.com>
2023-06-28 16:06:57 +0300

    Added comment

--- Untitled11.py
+++ Untitled11.py
@@ -8,7 +8,7 @@ import pandas as pd
 import streamlit as st
 import openpyxl
 
-# Read the CSV file
+# Read the CSV file (change dir below)
 data = pd.read_excel('Matrice Afidium copie.xlsx', sheet_name='RAW DATA1')
 
 
